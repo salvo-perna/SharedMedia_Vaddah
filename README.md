8d42e3b730bdc9c1d05cd47ae9ae6f98ec1d6881
Salvatore Perna <salvo.perna@gmail.com>
2023-05-06 14:23:00 +0200

    v1.0

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # SharedMedia_Vaddah
-A fork of SharedMedia for custom sounds/textures
+
+This is basically a fork of [SharedMedia] (https://github.com/bkader/SharedMedia). Most of the sounds in this pack are in italian.
+
+_ALL CREDITS GO TO FILES OWNERS_
+
+## Current Status
+
+| Media      |      Count    |
+|------------|:-------------:|
+| Textures   |     **01**    |
+| Sound      |     **27**    |
+
---
+++ SharedMedia_Vaddah/SharedMedia_Vaddah.lua
@@ -0,0 +1,45 @@
+-- tab size is 4
+-- registrations for media from the client itself belongs in LibSharedMedia-3.0
+
+local LSM = LibStub("LibSharedMedia-3.0")
+local koKR, ruRU, zhCN, zhTW, western = LSM.LOCALE_BIT_koKR, LSM.LOCALE_BIT_ruRU, LSM.LOCALE_BIT_zhCN, LSM.LOCALE_BIT_zhTW, LSM.LOCALE_BIT_western
+
+local MediaType_BACKGROUND = LSM.MediaType.BACKGROUND 
+local MediaType_BORDER = LSM.MediaType.BORDER 
+local MediaType_FONT = LSM.MediaType.FONT 
+local MediaType_STATUSBAR = LSM.MediaType.STATUSBAR 
+local MediaType_SOUND = LSM.MediaType.SOUND 
+
+-- -----------
+-- SOUNDS (27)
+-- -----------
+
+LSM:Register(MediaType_SOUND, "Vaddah: Bravo", [[Interface\AddOns\SharedMedia_Vaddah\media\sounds\Bravo.ogg]])
+LSM:Register(MediaType_SOUND, "Vaddah: Coccococo", [[Interface\AddOns\SharedMedia_Vaddah\media\sounds\Coccococo.ogg]])
+LSM:Register(MediaType_SOUND, "Vaddah: Coccodeco", [[Interface\AddOns\SharedMedia_Vaddah\media\sounds\Coccodeco.ogg]])
+LSM:Register(MediaType_SOUND, "Vaddah: Croccante", [[Interface\AddOns\SharedMedia_Vaddah\media\sounds\Croccante.ogg]])
+LSM:Register(MediaType_SOUND, "Vaddah: Dietro jhonny", [[Interface\AddOns\SharedMedia_Vaddah\media\sounds\Dietro jhonny.ogg]])
+LSM:Register(MediaType_SOUND, "Vaddah: Fuggi", [[Interface\AddOns\SharedMedia_Vaddah\media\sounds\Fuggi.ogg]])
+LSM:Register(MediaType_SOUND, "Vaddah: GUARAGNAU", [[Interface\AddOns\SharedMedia_Vaddah\media\sounds\GUARAGNAU.ogg]])
+LSM:Register(MediaType_SOUND, "Vaddah: Jhonny Boppami", [[Interface\AddOns\SharedMedia_Vaddah\media\sounds\Jhonny Boppami.ogg]])
+LSM:Register(MediaType_SOUND, "Vaddah: Jhonny sono immortale", [[Interface\AddOns\SharedMedia_Vaddah\media\sounds\Jhonny sono immortale.ogg]])
+LSM:Register(MediaType_SOUND, "Vaddah: La disfatta", [[Interface\AddOns\SharedMedia_Vaddah\media\sounds\La disfatta.ogg]])
+LSM:Register(MediaType_SOUND, "Vaddah: Le noltlend", [[Interface\AddOns\SharedMedia_Vaddah\media\sounds\Le noltlend.ogg]])
+LSM:Register(MediaType_SOUND, "Vaddah: Leoni e cani", [[Interface\AddOns\SharedMedia_Vaddah\media\sounds\Leoni e cani.ogg]])
+LSM:Register(MediaType_SOUND, "Vaddah: Ma guarda sto mongolo", [[Interface\AddOns\SharedMedia_Vaddah\media\sounds\Ma guarda sto mongolo.ogg]])
+LSM:Register(MediaType_SOUND, "Vaddah: Mammabombom", [[Interface\AddOns\SharedMedia_Vaddah\media\sounds\Mammabombom.ogg]])
+LSM:Register(MediaType_SOUND, "Vaddah: Nice, [[Interface\AddOns\SharedMedia_Vaddah\media\sounds\Nice.ogg]])
+LSM:Register(MediaType_SOUND, "Vaddah: Oh mammini", [[Interface\AddOns\SharedMedia_Vaddah\media\sounds\Oh mammini.ogg]])
+LSM:Register(MediaType_SOUND, "Vaddah: Only for exit", [[Interface\AddOns\SharedMedia_Vaddah\media\sounds\Only for exit.ogg]])
+LSM:Register(MediaType_SOUND, "Vaddah: Orcodi", [[Interface\AddOns\SharedMedia_Vaddah\media\sounds\Orcodi.ogg]])
+LSM:Register(MediaType_SOUND, "Vaddah: Scappa Jhonny figa", [[Interface\AddOns\SharedMedia_Vaddah\media\sounds\Scappa Jhonny figa.ogg]])
+LSM:Register(MediaType_SOUND, "Vaddah: Sono invincibile", [[Interface\AddOns\SharedMedia_Vaddah\media\sounds\Sono invincibile.ogg]])
+LSM:Register(MediaType_SOUND, "Vaddah: Spingilaaa", [[Interface\AddOns\SharedMedia_Vaddah\media\sounds\Spingilaaa.ogg]])
+LSM:Register(MediaType_SOUND, "Vaddah: Uiiii", [[Interface\AddOns\SharedMedia_Vaddah\media\sounds\Uiiii.ogg]])
+LSM:Register(MediaType_SOUND, "Vaddah: Uueeeueueue", [[Interface\AddOns\SharedMedia_Vaddah\media\sounds\Uueeeueueue.ogg]])
+LSM:Register(MediaType_SOUND, "Vaddah: Uuuh eeeh", [[Interface\AddOns\SharedMedia_Vaddah\media\sounds\Uuuh eeeh.ogg]])
+LSM:Register(MediaType_SOUND, "Vaddah: VIENI MERDA", [[Interface\AddOns\SharedMedia_Vaddah\media\sounds\VIENI MERDA.ogg]])
+LSM:Register(MediaType_SOUND, "Vaddah: Vai Verme", [[Interface\AddOns\SharedMedia_Vaddah\media\sounds\Vai Verme.ogg]])
+LSM:Register(MediaType_SOUND, "Vaddah: We ochio", [[Interface\AddOns\SharedMedia_Vaddah\media\sounds\We ochio.ogg]])
+LSM:Register(MediaType_SOUND, "Vaddah: Y are u gae?", [[Interface\AddOns\SharedMedia_Vaddah\media\sounds\Y are u gae.ogg]])
+
